292d36d5b12aa2cf62de7bc0016767e4b23ca142
Shakti Priya <priyashakti68@gmail.com>
2025-06-20 23:17:33 +0530

    update project configuration: add dotenv and colors dependencies, enhance server logging, and update .gitignore

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import express from 'express'; //import syntax is of module js
 //default is require syntax(common js)
+import dotenv from 'dotenv'
+import colors from 'colors'
+
+//below line means we are calling .env file in our application and our appln configure ho chuka ha
+dotenv.config();
 
 //rest object
 const app = express();
@@ -9,8 +14,8 @@ app.get('/', (req, res) => {
     res.send('<h1>Welcome to OUR JOB PORTAL !</h1>')
 })
 
-
+const port = process.env.PORT || 8080;
 //listen
-app.listen(8080, () => {
-    console.log('node server running on port 8080');
+app.listen(port, () => {
+    console.log(`node server is running in ${process.env.DEV_MODE} mode on port ${port}`.bgBlue.black);
 })
